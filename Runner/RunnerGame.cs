aa491cbccf57be0416a9b38939502ffa145e5370
Jay Harris <jay.harris@outlook.co.nz>
2016-07-14 21:25:03 +1200

    Has the program load a basic map

--- Runner/RunnerGame.cs
+++ Runner/RunnerGame.cs
@@ -1,11 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
+using Editor;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Input;
 using MultiPlayer;
 using MultiPlayer.Core;
+using Newtonsoft.Json;
 using Runner.Builders;
 
 namespace Runner
@@ -32,12 +35,21 @@ namespace Runner
             camera = CameraBuilder.CreateCamera(player.Transform);
 
             PrefabFactory.Instantiate(camera);
-            PrefabFactory.Instantiate("ground", new Vector2(6.5f, 7), 0, new Vector2(13, 1));
-            PrefabFactory.Instantiate("ground", new Vector2(0.5f, 4), 0, new Vector2(1, 8));
-            PrefabFactory.Instantiate("ground", new Vector2(12f, 4), 0, new Vector2(1, 8));
 
-            PrefabFactory.Instantiate("ground", new Vector2(11, 5.5f), 0, new Vector2(2, 5));
-            PrefabFactory.Instantiate("ground", new Vector2(1, 5.5f), 0, new Vector2(2, 5));
+            string json;
+            using (var r = new StreamReader(File.OpenRead("map.json")))
+            {
+                json = r.ReadToEnd();
+            }
+            var map = JsonConvert.DeserializeObject<MapInfo>(json);
+            PrefabFactory.Instantiate(map.Scene);
+
+            //PrefabFactory.Instantiate("ground", new Vector2(6.5f, 7), 0, new Vector2(13, 1));
+            //PrefabFactory.Instantiate("ground", new Vector2(0.5f, 4), 0, new Vector2(1, 8));
+            //PrefabFactory.Instantiate("ground", new Vector2(12f, 4), 0, new Vector2(1, 8));
+
+            //PrefabFactory.Instantiate("ground", new Vector2(11, 5.5f), 0, new Vector2(2, 5));
+            //PrefabFactory.Instantiate("ground", new Vector2(1, 5.5f), 0, new Vector2(2, 5));
         }
     }
 }
