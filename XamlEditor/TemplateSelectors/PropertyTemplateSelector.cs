f14a77b6c948098faabca2ddd57698ec53b2a91a
Jay Harris <jay.harris@outlook.co.nz>
2016-08-14 01:20:55 +1200

    Makes it possible to edit the position of GameObjects

--- XamlEditor/TemplateSelectors/PropertyTemplateSelector.cs
+++ XamlEditor/TemplateSelectors/PropertyTemplateSelector.cs
@@ -5,19 +5,23 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
+using XamlEditor.Properties;
 using XamlEditor.ViewModels;
+using XamlEditor.ViewModels.PropertySheets;
 
 namespace XamlEditor.TemplateSelectors
 {
     public class PropertyTemplateSelector : DataTemplateSelector
     {
-        private readonly Type[] numerics = {
+        private static readonly Type[] Numerics = {
             typeof(int),
             typeof(long),
             typeof(byte),
             typeof(float),
             typeof(double)
         };
+
+        public DataTemplate UnknownDataTemplate { get; set; }
 
         public DataTemplate ArrayDataTemplate { get; set; }
 
@@ -26,17 +30,26 @@ namespace XamlEditor.TemplateSelectors
         public DataTemplate NumericDataTemplate { get; set; }
         public DataTemplate StringDataTemplate { get; set; }
         public DataTemplate BoolDataTemplate { get; set; }
+
+        public DataTemplate Vector2DataTemplate { get; set; }
         
         public override DataTemplate SelectTemplate(object item, DependencyObject container)
         {
-            var type = (item as PrimitiveViewModel)?.ValueType ?? item?.GetType();
+            var type = item?.GetType();
             if (type == null) return base.SelectTemplate(null, container);
 
-            if (type == typeof(string) && StringDataTemplate != null) return StringDataTemplate;
-            if (numerics.Contains(type) && NumericDataTemplate != null) return NumericDataTemplate;
-            if (type == typeof(bool) && BoolDataTemplate != null) return BoolDataTemplate;
+            if (item is PrimitiveViewModel)
+            {
+                type = ((PrimitiveViewModel) item).ValueType;
+                if (type == typeof(string) && StringDataTemplate != null) return StringDataTemplate;
+                if (Numerics.Contains(type) && NumericDataTemplate != null) return NumericDataTemplate;
+                if (type == typeof(bool) && BoolDataTemplate != null) return BoolDataTemplate;
+            }
 
-            return ComplexDataTemplate ?? base.SelectTemplate(item, container);
+            if (item is Vector2ViewModel)
+                return Vector2DataTemplate;
+
+            return UnknownDataTemplate ?? base.SelectTemplate(item, container);
         }
     }
 }
