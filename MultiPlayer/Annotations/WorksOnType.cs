2ff15bbf8417dae10a96b4b8dba1b3fdba5ca2ef
fallaciousreasoning <jay.harris@outlook.co.nz>
2016-09-01 13:39:27 +1200

    Adds a works on attribute

---
+++ MultiPlayer/Annotations/WorksOnType.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MultiPlayer.Annotations
+{
+    [AttributeUsage(AttributeTargets.Class, AllowMultiple = false, Inherited = false)]
+    public class WorksOnType : Attribute
+    {
+        public Type Expects { get; }
+        public List<Type> Composition { get; } = new List<Type>();
+        public WorksOnType(Type expects)
+        {
+            Expects = expects;
+
+            ExtractTypes();
+        }
+
+        private void ExtractTypes()
+        {
+            var fields = Expects.GetFields();
+
+            foreach (var field in fields)
+            {
+                if (field.IsLiteral || field.IsInitOnly || !field.IsPublic) continue;
+                Composition.Add(field.FieldType);
+            }
+        }
+    }
+}
