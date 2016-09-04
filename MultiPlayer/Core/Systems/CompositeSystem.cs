316921f371204d78f49fe69d041ab38bb25972fb
Jay Harris <jay.harris@outlook.co.nz>
2016-09-04 16:43:43 +1200

    Makes the composite system a little more useful

--- MultiPlayer/Core/Systems/CompositeSystem.cs
+++ MultiPlayer/Core/Systems/CompositeSystem.cs
@@ -3,26 +3,35 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using MultiPlayer.Annotations;
 using MultiPlayer.Collections;
 using MultiPlayer.Core.Families;
 using MultiPlayer.Core.Messaging;
 
 namespace MultiPlayer.Core.Systems
 {
-    public class CompositeSystem<T> : ISystem, IRequiresFamily, IHearsMessageTypes
+    [HearsMessage(typeof(StartMessage))]
+    public class CompositeSystem<T> : ISystem, IRequiresFamily, IHearsMessageTypes, IKnowsEngine
     {
-        public NodeFamily<T> Nodes { get; private set; }
+        public INodeFamily<T> Nodes { get; private set; }
         private Dictionary<Type, ISystem> Systems { get; } = new Dictionary<Type, ISystem>();
+
+        protected void Start()
+        {
+            Nodes = Engine.FamilyManager.GetNodeFamily<T>();
+        }
 
         public void RecieveMessage(IMessage message)
         {
+            if (message is StartMessage) Start();
+
             var messageType = message.GetType();
 
             if (Systems.ContainsKey(messageType))
                 Systems[messageType].RecieveMessage(message);
         }
 
-        public CompositeSystem<T> WithSystem<K>(SubSystem<K> system) where K : IMessage
+        public CompositeSystem<T> WithSystem<K>(SubSystem<T, K> system) where K : IMessage
         {
             Systems.Add(typeof(K), system);
             HearsMessages.Add(typeof(K));
@@ -31,5 +40,6 @@ namespace MultiPlayer.Core.Systems
         
         public IList<Type> HearsMessages { get; } = new List<Type>();
         public Type FamilyType { get; } = typeof(T);
+        public Engine Engine { get; set; }
     }
 }
--- MultiPlayer/Core/Systems/SubSystem.cs
+++ MultiPlayer/Core/Systems/SubSystem.cs
@@ -7,14 +7,16 @@ using MultiPlayer.Core.Messaging;
 
 namespace MultiPlayer.Core.Systems
 {
-    public abstract class SubSystem<T> : ISystem where T : IMessage
+    public abstract class SubSystem<T1, T2> : ISystem where T2 : IMessage
     {
+        public CompositeSystem<T1> Owner { get; set; }
+
         public void RecieveMessage(IMessage message)
         {
             //Just trust this has been handled by the composite system
-            Handle((T)message);
+            Handle((T2)message);
         }
 
-        public abstract void Handle(T message);
+        public abstract void Handle(T2 message);
     }
 }
