e255315a957087293eeec089920262c7549faf73
Jay Harris <jay.harris@outlook.co.nz>
2017-01-11 17:18:32 +1300

    Begins work on a new system for prefabs

---
+++ MultiPlayer/Factories/BasicPrefab.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MultiPlayer.Core;
+
+namespace MultiPlayer.Factories
+{
+ public class BasicPrefab : IPrefab
+ {
+  private readonly Func<EntityBuilder> builder;
+
+  public BasicPrefab(Func<EntityBuilder> builder)
+  {
+   this.builder = builder;
+  }
+
+  public List<Entity> Build()
+  {
+   return builder().Create();
+  }
+ }
+}
