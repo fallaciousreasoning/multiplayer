8f6d61cf3b13efdf4133199038a71bf6cdc0cea7
Jay Harris <jay.harris@outlook.co.nz>
2017-01-23 20:48:11 +1300

    Adds some basic code to the simplesystem

--- MultiPlayer/Core/Systems/SimpleSystem.cs
+++ MultiPlayer/Core/Systems/SimpleSystem.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MultiPlayer.Core.Messaging;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,24 +7,30 @@ using System.Threading.Tasks;
 
 namespace MultiPlayer.Core.Systems
 {
-    public abstract class SimpleSystem<T>
+    public abstract class SimpleSystem<T> : ISystem, IKnowsEngine
     {
-        public virtual void OnEntityAdded(Entity entity)
+        private Dictionary<Type, Action<IMessage>> messageHandlers = new Dictionary<Type, Action<IMessage>>();
+
+        protected List<T> Nodes { get; private set; }
+
+        public Engine Engine { get; set; }
+
+        public virtual void OnNodeAdded(T node)
         {
 
         }
 
-        public virtual void OnEntityRemoved(Entity entity)
+        public virtual void OnNodeRemoved(T node)
         {
 
         }
 
-        public virtual void OnComponentAdded(Entity entity, object component)
+        public virtual void OnComponentAdded(Entity node, object component)
         {
 
         }
 
-        public virtual void OnComponentRemoved(Entity entity, object component)
+        public virtual void OnComponentRemoved(Entity node, object component)
         {
 
         }
@@ -61,6 +68,60 @@ namespace MultiPlayer.Core.Systems
         public virtual void Draw(T node)
         {
 
+        }
+
+        public virtual void OnUnhandledMessage(IMessage message)
+        {
+
+        }
+
+        public void RecieveMessage(IMessage message)
+        {
+            if (message is StartMessage)
+                Nodes.ForEach(Start);
+            else if (message is UpdateMessage)
+                Nodes.ForEach(Update);
+            else if (message is DrawMessage)
+                Nodes.ForEach(Draw);
+            else if (message is ComponentAddedMessage)
+            {
+                var m = (ComponentAddedMessage)message;
+                OnComponentAdded(m.Target, m.Component);
+            }
+            else if (message is ComponentRemovedMessage)
+            {
+                var m = (ComponentRemovedMessage)message;
+                OnComponentRemoved(m.Target, m.Component);
+            }
+            else if (message is CollisionMessage)
+            {
+                var m = (CollisionMessage)message;
+                if (m.IsTrigger)
+                {
+                    if (m.Mode == CollisionMode.Entered)
+                        OnTriggerEnter(m.Target, m.Hit);
+                    else OnTriggerExit(m.Target, m.Hit);
+                }
+                else
+                {
+                    if (m.Mode == CollisionMode.Entered)
+                        OnCollisionEnter(m.Target, m.Hit);
+                    else OnCollisionExit(m.Target, m.Hit);
+                }
+            }
+            else if (messageHandlers.ContainsKey(message.GetType()))
+                messageHandlers[message.GetType()](message);
+            else OnUnhandledMessage(message);
+        }
+
+        public void RegisterHandler<MessageType>(Action<MessageType> handler)
+            where MessageType : IMessage
+        {
+            var messageType = typeof(MessageType);
+            if (messageHandlers.ContainsKey(messageType)) throw new Exception("There is already a handler for " + messageType.Name);
+
+            Action<IMessage> wrapper = (m) => handler((MessageType)m);
+            messageHandlers.Add(messageType, wrapper);
         }
     }
 }
