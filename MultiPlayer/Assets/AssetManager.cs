14503050d4560911d3163d4110289e06c7444e8b
Jay Harris <jay.harris@outlook.co.nz>
2017-01-27 21:50:23 +1300

    Stubs an asset manager

---
+++ MultiPlayer/Assets/AssetManager.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework.Content;
+
+namespace MultiPlayer.Assets
+{
+    public class AssetManager
+    {
+        private readonly Dictionary<string, (int count, object asset)> assetTracker = new Dictionary<string, ValueTuple<int, object>>();
+
+        public AssetManager()
+        {
+            
+        }
+    }
+}
